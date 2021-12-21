1e394c126bc98dcced9bd4b7c8809dd17bf06b01
waigoma <waichi0302@gmail.com>
2021-12-22 00:56:44 +0900

    first commit

---
+++ src/main/java/com/waigoma/kotlintutorial/KotlinTutorialPlugin.kt
@@ -0,0 +1,13 @@
+package com.waigoma.kotlintutorial
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KotlinTutorialPlugin : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
